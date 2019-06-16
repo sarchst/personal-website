bc64555fdc953cd0ac6c1394b9620cc334831e8e
sarch <sarchenstarke@gmail.com>
2019-06-15 18:19:35 -0700

    Added some resume content, and styled the resume page

--- app/public/wp-content/themes/sarchen-theme/resume.php
+++ app/public/wp-content/themes/sarchen-theme/resume.php
@@ -10,9 +10,37 @@ get_header();
 </div>
 <div class="quicklook">
     <h2>A Quick Look</h2>
+    <div class="three-grid">
+        <div class="summary">
+            <h3>Summary</h3>
+            <p>
+                <span>&#8226;</span> Fourth year, Bachelor of Computer Science student<br>
+                <span>&#8226;</span> Prior Java, JavaScript, PHP internship experience 
+            </p>
+        </div>
+        <div class="languages">
+            <h3>Languages</h3>
+            <p>Java, Python, JavaScript, C++, XML, PHP</p>
+        </div>
+        <div class="tools-and-frameworks">
+            <h3>Tools and Frameworks</h3>
+            <p>Android Studio, React Native, Git, MySQL, Bootstrap, Flask, SQLite, IntelliJ, NetBeans</p>
+        </div>
+    </div>
 </div>
 <div class="coop">
     <h2>Co-op Experience</h2>
+    <div class="two-grid">
+        <div>
+            <h3>Evident Point</h3>
+            test1
+        </div>
+        <div>
+            <h3>VeloMetro</h3>
+            test2
+        </div>
+
+    </div>
 </div>
 <div class="other-experience">
     <h2>Other Work Experience</h2>
