19bed36123a59fce45974868d0d5ea97c008c398
sarch <sarchenstarke@gmail.com>
2019-08-11 21:00:56 -0700

    Listings from my Etsy are now displayed using Etsy API, style updates to shop page

--- app/public/wp-content/themes/sarchen-theme/etsy.php
+++ app/public/wp-content/themes/sarchen-theme/etsy.php
@@ -1,30 +1,40 @@
 <?php
-/*
-Template Name: Etsy
-*/
-get_header();
+    /*
+    Template Name: Etsy
+    */
+    get_header();
+    include 'api.php';
+?>
+
+<?php
+    $url = "https://openapi.etsy.com/v2/shops/6981955/listings/active?api_key=" . $API . "&fields=url,title,price&includes=MainImage";
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response_body = curl_exec($ch);
+    $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if (intval($status) != 200) throw new Exception("HTTP $status\n$response_body");
+    $response = json_decode($response_body);
 ?>
 
 <div class="listings">
     <div class="three-grid">
-        <?php 
-        $listings = CFS()->get( 'listings' );
-        foreach ($listings as $listing) {
-        ?>
+    <?php foreach ($response->results as $listing) { 
+        // regex to shorten the listing titles
+        $temp = preg_replace("/(?<= - ).*$/", "", $listing->title);
+        $processedTitle = preg_replace("/( - )/", "", $temp); ?>
 
         <div class="etsy">
-            <h3><?php echo $listing['title']; ?></h3>
-            <p><?php echo $listing['description']; ?></p>
-            <?php echo $listing['url']; ?>
-            <img src='<?php echo $listing['photo']; ?>' alt='listing-image'>
-            
+            <h3><?php echo $processedTitle; ?></h3><br>
+            <img src=<?php echo $listing->MainImage->url_170x135 ?> >
+            <p>$<?php echo $listing->price; ?> (CAD)</p>
+            <a href="<?php echo $listing->url; ?>">Buy Here &#9679; More Information</a>
         </div>
 
-        
-            <?php
-            }
-        ?>
-        
-        </div>
+    <?php } ?>
     </div>
 </div>
+
+<?php
+get_footer();
+?>
