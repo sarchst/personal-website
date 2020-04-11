5ddb0b2d256669754538b16c90a3611f932b3c07
Sarchen Starke <sarchenstarke@gmail.com>
2020-04-11 15:26:11 -0700

    small fix

--- etsy.php
+++ etsy.php
@@ -13,20 +13,20 @@
 
 <?php
     // Ouiware request
-    function requestOuiwareListings() {
-        $url = "https://openapi.etsy.com/v2/shops/6981955/listings/active?api_key=bsl1gn1vjoiziv8067u7uzed&fields=url,title,price&includes=MainImage";
+    // function requestOuiwareListings() {
+    //     $url = "https://openapi.etsy.com/v2/shops/6981955/listings/active?api_key=" API GOES HERE" &fields=url,title,price&includes=MainImage";
 
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response_body = curl_exec($ch);
-        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if (intval($status) != 200) throw new Exception("HTTP $status\n$response_body");
-        return json_decode($response_body);
-    }
+    //     $ch = curl_init($url);
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //     $response_body = curl_exec($ch);
+    //     $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    //     if (intval($status) != 200) throw new Exception("HTTP $status\n$response_body");
+    //     return json_decode($response_body);
+    // }
 
     // DANGITJANET request
     // function requestDangitListings() {
-    //     $url = "https://openapi.etsy.com/v2/shops/19106942/listings/active?api_key=bsl1gn1vjoiziv8067u7uzed&fields=url,title,price&includes=MainImage";
+    //     $url = "https://openapi.etsy.com/v2/shops/19106942/listings/active?api_key=" API GOES HERE "&fields=url,title,price&includes=MainImage";
 
     //     $ch = curl_init($url);
     //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
